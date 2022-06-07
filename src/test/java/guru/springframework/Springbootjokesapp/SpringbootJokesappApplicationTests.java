7d0981ab7b489ef65a0ac0c69f54a3beb7571d5e
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-06-07 14:27:43 +0300

    Created the project with spring initialzr

---
+++ src/test/java/guru/springframework/Springbootjokesapp/SpringbootJokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.Springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
